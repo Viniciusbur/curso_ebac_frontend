61297488a4ffe9ab8fd439d891555c3da50db889
Vinicius Burgie <Viniciusburgie@gmail.com>
2023-10-03 14:46:02 -0300

    Atualizacao
    Atualizacao dos funcionalidades

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const form = document.getElementById('form-validacao');
 
 function validaValores(a, b){
-if(a>b){
+if(b>a){
     return true;
 }else{
     return false;
@@ -9,18 +9,18 @@ if(a>b){
 
 form.addEventListener('submit', function(e){
     e.preventDefault();
+    const limpaTela = document.querySelector('.sucess-message').innerHTML = '';
     
     const valorA = document.getElementById('numero-A');
     const valorB = document.getElementById('numero-B');
 
-    const mensagemSucesso = `O valor de A <b>${valorA}</b> é maior que valor de B <b>${valorB}</b>`;
+    const mensagemSucesso = `O valor de B <b>(${valorB.value})</b> é maior que valor de A <b>(${valorA.value})</b>`;
 
-    if (validaValores(valorA,valorB)){
+    if (validaValores(valorA.value,valorB.value)){
         document.querySelector('.sucess-message').innerHTML = mensagemSucesso;
     }else{
-        alert("O valor A é inferior ao valor B");
+        alert("O valor B é inferior ao valor A, favor informe outro valor");
     }
-
-})
-
-console.log(form);
+    valorA.value = null;
+    valorB.value = null;
+});
