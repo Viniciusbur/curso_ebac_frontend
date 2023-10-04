28ee9cee2f1e38c09277b72562a16ddce14c683a
Vinicius Burgie <Viniciusburgie@gmail.com>
2023-10-04 10:59:45 -0300

    Aleteracoes
    Algumas correções, conversao para float

--- main.js
+++ main.js
@@ -1,26 +1,24 @@
 const form = document.getElementById('form-validacao');
 
 function validaValores(a, b){
-if(b>a){
-    return true;
-}else{
-    return false;
-}}
+    return Number(b) > Number(a);
+}
 
 form.addEventListener('submit', function(e){
     e.preventDefault();
     const limpaTela = document.querySelector('.sucess-message').innerHTML = '';
     
-    const valorA = document.getElementById('numero-A');
-    const valorB = document.getElementById('numero-B');
+    const valorA = parseFloat(document.getElementById('numero-A').value);
+    const valorB = parseFloat(document.getElementById('numero-B').value);
 
-    const mensagemSucesso = `O valor de B <b>(${valorB.value})</b> é maior que valor de A <b>(${valorA.value})</b>`;
+    const mensagemSucesso = `O valor de B <b>(${valorB})</b> é maior que valor de A <b>(${valorA})</b>`;
 
-    if (validaValores(valorA.value,valorB.value)){
+    if (validaValores(valorA,valorB)){
         document.querySelector('.sucess-message').innerHTML = mensagemSucesso;
     }else{
         alert("O valor B é inferior ao valor A, favor informe outro valor");
     }
-    valorA.value = null;
-    valorB.value = null;
+
+    document.getElementById('numero-A').value = null;
+    document.getElementById('numero-B').value = null;
 });
