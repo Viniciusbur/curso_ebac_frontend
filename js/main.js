92b9ca2faca0dab44ee05432a698e615da8133cf
Vinicius Burgie <Viniciusburgie@gmail.com>
2024-03-12 17:14:55 -0300

    Realizado correcoes solicitadas

--- js/main.js
+++ js/main.js
@@ -3,8 +3,15 @@ $(document).ready(function(){
     $('#telefone').mask('(00) 00000-0000' , {
         placeholder: '(00) 00000-0000'
     })
+
+    $('#cpf').mask('000.000.000-00' , {
+        placeholder: '000.000.000-00'
+    })
+    $('#cep').mask('00000-000' , {
+        placeholder: '00000-000'
+    })
     
-    $('form').validade({
+    $('form').validate({
         rules: {
             nome:{
                 required:true
@@ -14,7 +21,14 @@ $(document).ready(function(){
                 email: true
             },telefone: {
                 required:true
+            },
+            cep:{
+                required:true
+            },
+            cpf:{
+                required:true
             }
+           
         }
     })
 })
